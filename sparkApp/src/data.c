8e7944dbb5eef41bf5f1108e55ee9619418a1169
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2023-09-07 15:29:37 +0300

    Used generic data protocol file for all types of waveforms.
    protocol/data.proto: Accepted waveform type as an argument.
    sparkApp/src/data.c: Fixed unkown pointer bug in 8-way split.

--- sparkApp/src/data.c
+++ sparkApp/src/data.c
@@ -48,12 +48,33 @@ static long split_8_way(aSubRecord* record)
 {
 	long i;
 	short* data_array;
-	short* channel_ptr;
+	short* channel_1;
+	short* channel_2;
+	short* channel_3;
+	short* channel_4;
+	short* channel_5;
+	short* channel_6;
+	short* channel_7;
+	short* channel_8;
 
 	data_array  = (short*) record->a;
-	channel_ptr = record->vala;
-	for(i = 0; i < record->nea; i++) {
-		((short*)(channel_ptr + i % 8))[i / 8] = data_array[i];
+	channel_1 = record->vala;
+	channel_2 = record->valb;
+	channel_3 = record->valc;
+	channel_4 = record->vald;
+	channel_5 = record->vale;
+	channel_6 = record->valf;
+	channel_7 = record->valg;
+	channel_8 = record->valh;
+	for(i = 0; i < record->nea; i += 4) {
+		channel_1[i / 8] = data_array[i];
+		channel_2[i / 8] = data_array[i + 1];
+		channel_3[i / 8] = data_array[i + 2];
+		channel_4[i / 8] = data_array[i + 3];
+		channel_5[i / 8] = data_array[i + 4];
+		channel_6[i / 8] = data_array[i + 5];
+		channel_7[i / 8] = data_array[i + 6];
+		channel_8[i / 8] = data_array[i + 7];
 	}
 
 	return 0;
